f6b59819d42ceadcff71b8164962f467fa16e6b3
prade <prade@LAPTOP-P4C2EFM2.home>
2018-01-11 15:46:56 -0500

    hotfix

--- publish_docker_image_to_AWS_ECS.sh
+++ publish_docker_image_to_AWS_ECS.sh
@@ -2,7 +2,9 @@
 echo "Running docker build script"
 echo "$1"
 DOCKER_LOGIN=`aws ecr get-login --no-include-email --region us-east-1`
-${DOCKER_LOGIN}
+echo "$DOCKER_LOGIN"
+LOGIN_COMMAND=${DOCKER_LOGIN}
+LOGIN_COMMAND
 
 sudo docker tag cassandra-webapp:$1 468913107500.dkr.ecr.us-east-1.amazonaws.com/pradeep-ecs-repo:cassandra-webapp.latest
 sudo docker push 468913107500.dkr.ecr.us-east-1.amazonaws.com/pradeep-ecs-repo:cassandra-webapp.latest
